8fbec6ed95ec735fbfcfde056b40fdcbaea0036c
Epiphany0130 <sdguyq@163.com>
2026-03-15 07:33:36 +0800

    新建项目

---
+++ src/main/java/com/gyqstd/visualassets/VisualAssetsApplication.java
@@ -0,0 +1,13 @@
+package com.gyqstd.visualassets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualAssetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisualAssetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gyqstd/visualassets/VisualAssetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyqstd.visualassets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualAssetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
